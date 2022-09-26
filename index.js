adea28e81d2fb4ed15394afa2a5395b72573e24c
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2022-09-26 20:06:25 -0300

    :tada: initial setup

---
+++ index.js
@@ -0,0 +1,31 @@
+const { response } = require("express");
+const express = require("express");
+const path = require("path");
+
+const port = 5000;
+
+const server = express();
+
+const basePath = path.join(__dirname, "templates");
+
+server.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+server.use(express.json());
+
+server.use(express.static("public"));
+
+server.get("/", (request, response) => {
+  response.sendFile(`${basePath}/index.html`);
+});
+
+server.use((request, response, next) => {
+  response.status(404).sendFile(`${basePath}/notFound.html`);
+});
+
+server.listen(port, () => {
+  console.log(`Server listen on port: ${port}`);
+});
